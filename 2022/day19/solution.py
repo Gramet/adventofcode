a2aea81d8ecade58fcad9ddb8813ff044b77657f
Gaétan Ramet <gr@visium.ch>
2022-12-20 15:55:39 +0100

    2022 day 19-20

---
+++ 2022/day19/solution.py
@@ -0,0 +1,201 @@
+from pathlib import Path
+import re
+from copy import deepcopy
+from math import ceil
+
+ORE = 0
+CLAY = 1
+OBSIDIAN = 2
+GEODE = 3
+
+
+def eval_blueprint(blueprint, time_left, robots, items, answer, score):
+    state_key = f"{robots}-{items}-{time_left}"
+    answer[state_key] = max(answer.get(state_key, 0), score)
+    if time_left == 1:
+        return answer
+    max_reachable = score + time_left * (time_left - 1) / 2
+    if max_reachable <= max(answer.values()):
+        return answer
+
+    # Build Geode robot
+    if robots[OBSIDIAN]:
+        geode_cost = blueprint["geode"]
+        time_to_build = max(
+            ceil((geode_cost[1] - items[OBSIDIAN]) / robots[OBSIDIAN]) + 1,
+            ceil((geode_cost[0] - items[ORE]) / robots[ORE]) + 1,
+            1,
+        )
+        if time_to_build < time_left:
+            new_time_left = time_left - time_to_build
+            new_robots = deepcopy(robots)
+            new_items = deepcopy(items)
+            new_robots[GEODE] += 1
+            new_items[ORE] += time_to_build * robots[ORE] - geode_cost[0]
+            new_items[CLAY] += time_to_build * robots[CLAY]
+            new_items[OBSIDIAN] += time_to_build * robots[OBSIDIAN] - geode_cost[1]
+            new_items[GEODE] += time_to_build * robots[GEODE]
+
+            eval_blueprint(
+                blueprint,
+                new_time_left,
+                new_robots,
+                new_items,
+                answer,
+                score + new_time_left,
+            )
+
+    # Build Obsidian robot
+    if robots[CLAY] and (robots[OBSIDIAN] < blueprint["geode"][1]):
+        obs_cost = blueprint["obsidian"]
+        time_to_build = max(
+            ceil((obs_cost[1] - items[CLAY]) / robots[CLAY]) + 1,
+            ceil((obs_cost[0] - items[ORE]) / robots[ORE]) + 1,
+            1,
+        )
+        if time_to_build < time_left:
+            new_time_left = time_left - time_to_build
+            new_robots = deepcopy(robots)
+            new_items = deepcopy(items)
+            new_robots[OBSIDIAN] += 1
+            new_items[ORE] += time_to_build * robots[ORE] - obs_cost[0]
+            new_items[CLAY] += time_to_build * robots[CLAY] - obs_cost[1]
+            new_items[OBSIDIAN] += time_to_build * robots[OBSIDIAN]
+            new_items[GEODE] += time_to_build * robots[GEODE]
+
+            eval_blueprint(
+                blueprint,
+                new_time_left,
+                new_robots,
+                new_items,
+                answer,
+                score,
+            )
+
+    # Build Clay robot
+    if robots[CLAY] < blueprint["obsidian"][1]:
+        clay_cost = blueprint["clay"]
+        time_to_build = max(ceil((clay_cost - items[ORE]) / robots[ORE]) + 1, 1)
+        if time_to_build < time_left:
+            new_time_left = time_left - time_to_build
+            new_robots = deepcopy(robots)
+            new_items = deepcopy(items)
+            new_robots[CLAY] += 1
+            new_items[ORE] += time_to_build * robots[ORE] - clay_cost
+            new_items[CLAY] += time_to_build * robots[CLAY]
+            new_items[OBSIDIAN] += time_to_build * robots[OBSIDIAN]
+            new_items[GEODE] += time_to_build * robots[GEODE]
+
+            eval_blueprint(
+                blueprint,
+                new_time_left,
+                new_robots,
+                new_items,
+                answer,
+                score,
+            )
+
+    # Build Ore robot
+    if not (
+        robots[ORE]
+        >= max(
+            [
+                blueprint["ore"],
+                blueprint["clay"],
+                blueprint["obsidian"][0],
+                blueprint["geode"][0],
+            ]
+        )
+    ):
+        ore_cost = blueprint["ore"]
+        time_to_build = max(ceil((ore_cost - items[ORE]) / robots[ORE]) + 1, 1)
+        if time_to_build < time_left:
+            new_time_left = time_left - time_to_build
+            new_robots = deepcopy(robots)
+            new_items = deepcopy(items)
+            new_robots[ORE] += 1
+            new_items[ORE] += time_to_build * robots[ORE] - ore_cost
+            new_items[CLAY] += time_to_build * robots[CLAY]
+            new_items[OBSIDIAN] += time_to_build * robots[OBSIDIAN]
+            new_items[GEODE] += time_to_build * robots[GEODE]
+            eval_blueprint(
+                blueprint,
+                new_time_left,
+                new_robots,
+                new_items,
+                answer,
+                score,
+            )
+    return answer
+
+
+class Solution:
+    def __init__(self):
+        with open(Path(__file__).parent / "input", "r") as f:
+            self.input = f.readlines()
+        self.blueprints = {}
+        for line in self.input:
+            matches = re.search(
+                r"[^0-9]+(\d+)[^0-9]+(\d+)[^0-9]+(\d+)[^0-9]+(\d+)[^0-9]+(\d+)[^0-9]+(\d+)[^0-9]+(\d+)",
+                line,
+            )
+            self.blueprints[matches.group(1)] = {
+                "ore": int(matches.group(2)),
+                "clay": int(matches.group(3)),
+                "obsidian": (int(matches.group(4)), int(matches.group(5))),
+                "geode": (int(matches.group(6)), int(matches.group(7))),
+            }
+        print(self.blueprints)
+
+    def solve_part_1(self):
+
+        tot_quality = 0
+        for id, blueprint in self.blueprints.items():
+            answer = eval_blueprint(
+                blueprint,
+                time_left=24,
+                robots=[1, 0, 0, 0],
+                items=[0, 0, 0, 0],
+                answer={},
+                score=0,
+            )
+            max_val = max(answer.values())
+            answer = {k: v for k, v in answer.items() if v == max_val}
+            print(f"{int(id)}: quality {max_val}")
+            tot_quality += int(id) * max_val
+        answer = tot_quality
+        print(answer)
+        return answer
+
+    def solve_part_2(self):
+        tot = 1
+        for id, blueprint in self.blueprints.items():
+            if int(id) not in [1, 2, 3]:
+                continue
+            answer = eval_blueprint(
+                blueprint,
+                time_left=32,
+                robots=[1, 0, 0, 0],
+                items=[0, 0, 0, 0],
+                answer={},
+                score=0,
+            )
+            max_val = max(answer.values())
+            print(max_val)
+            tot *= max_val
+
+        answer = tot
+        print(answer)
+        return answer
+
+    def save_results(self):
+        with open(Path(__file__).parent / "part1", "w") as opened_file:
+            opened_file.write(str(self.solve_part_1()))
+
+        with open(Path(__file__).parent / "part2", "w") as opened_file:
+            opened_file.write(str(self.solve_part_2()))
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    solution.save_results()
---
+++ 2022/day20/solution.py
@@ -0,0 +1,185 @@
+from pathlib import Path
+from copy import deepcopy
+from tqdm import tqdm
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.previous = None
+
+    def __repr__(self):
+        return f"Node {self.data} - previous = {self.previous.data} - next = {self.next.data}"
+
+
+class CircularLinkedList:
+    def __init__(self):
+        self.head = None
+        self.len = 5000
+
+    def __iter__(self):
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
+
+    def traverse(self, starting_point=None):
+        if starting_point is None:
+            starting_point = self.head
+        node = starting_point
+        while node is not None and (node.next != starting_point):
+            yield node
+            node = node.next
+        yield node
+
+    def print_list(self, starting_point=None):
+        nodes = []
+        for node in self.traverse(starting_point):
+            nodes.append(str(node))
+        print(" -> ".join(nodes))
+
+    def remove_node(self, target_node_data):
+        if self.head is None:
+            raise Exception("List is empty")
+
+        if self.head.data == target_node_data:
+            node_to_remove = self.head
+            self.head.previous.next = self.head.next
+            self.head.next.previous = self.head.previous
+            self.head = self.head.next
+            return node_to_remove
+
+        for node in self:
+            if node.data == target_node_data:
+                node_to_remove = node
+                node.previous.next = node.next
+                node.next.previous = node.previous
+                return node_to_remove
+
+        raise Exception("Node with data '%s' not found" % target_node_data)
+
+    def move_right(self, target_node_data, steps):
+
+        if self.head is None:
+            raise Exception("List is empty")
+
+        for node in self:
+            if node.data == target_node_data:
+                for _ in range(steps):
+                    node_to_move = node
+                    node.previous.next = node.next
+                    node.next.previous = node.previous
+                    node.next.next.previous = node
+                    node.previous = node.next
+                    node.next = node.next.next
+                    node.previous.next = node
+                return node_to_move
+
+        raise Exception("Node with data '%s' not found" % target_node_data)
+
+    def move_left(self, target_node_data, steps):
+        if self.head is None:
+            raise Exception("List is empty")
+
+        for node in self:
+            if node.data == target_node_data:
+                for _ in range(steps):
+                    node_to_move = node
+                    node.next.previous = node.previous
+                    node.previous.next = node.next
+                    node.previous.previous.next = node
+                    node.next = node.previous
+                    node.previous = node.previous.previous
+                    node.next.previous = node
+                return node_to_move
+
+        raise Exception("Node with data '%s' not found" % target_node_data)
+
+    def move(self, target_node_data):
+        if target_node_data[1] > 0:
+            self.move_right(target_node_data, target_node_data[1] % (self.len - 1))
+        elif target_node_data[1] < 0:
+            self.move_left(target_node_data, abs(target_node_data[1]) % (self.len - 1))
+
+    def traverse_from(self, target_node_data, steps):
+        for node in self:
+            if node.data == target_node_data:
+                res_node = node
+                for _ in range(steps):
+                    res_node = res_node.next
+                return res_node.data[1]
+
+
+class Solution:
+    def __init__(self):
+        encryption_key = 811589153
+        input_file = "input"
+        with open(Path(__file__).parent / input_file, "r") as f:
+            self.input = [
+                Node((start_idx, int(x))) for start_idx, x in enumerate(f.readlines())
+            ]
+        with open(Path(__file__).parent / input_file, "r") as f:
+            self.encrypted_input = [
+                Node((start_idx, encryption_key * int(x)))
+                for start_idx, x in enumerate(f.readlines())
+            ]
+        for i in range(1, len(self.input)):
+            self.input[i].previous = self.input[i - 1]
+            self.encrypted_input[i].previous = self.encrypted_input[i - 1]
+        self.input[0].previous = self.input[-1]
+        self.encrypted_input[0].previous = self.encrypted_input[-1]
+        for i in range(0, len(self.input) - 1):
+            self.input[i].next = self.input[i + 1]
+            self.encrypted_input[i].next = self.encrypted_input[i + 1]
+        self.input[-1].next = self.input[0]
+        self.encrypted_input[-1].next = self.encrypted_input[0]
+        for n in self.input:
+            if n.data[1] == 0:
+                self.zero_node_data = n.data
+        for n in self.encrypted_input:
+            if n.data[1] == 0:
+                self.zero_node_data_encryted = n.data
+
+    def solve_part_1(self):
+        circ_list = CircularLinkedList()
+        circ_list.head = self.input[0]
+        for node in tqdm(self.input):
+            circ_list.move(node.data)
+
+        answer = (
+            circ_list.traverse_from(self.zero_node_data, 1000)
+            + circ_list.traverse_from(self.zero_node_data, 2000)
+            + circ_list.traverse_from(self.zero_node_data, 3000)
+        )
+
+        print(answer)
+        return answer
+
+    def solve_part_2(self):
+        circ_list = CircularLinkedList()
+        circ_list.head = self.encrypted_input[0]
+        for _ in range(10):
+            for node in tqdm(self.encrypted_input):
+                circ_list.move(node.data)
+
+        answer = (
+            circ_list.traverse_from(self.zero_node_data, 1000)
+            + circ_list.traverse_from(self.zero_node_data, 2000)
+            + circ_list.traverse_from(self.zero_node_data, 3000)
+        )
+
+        print(answer)
+        return answer
+
+    def save_results(self):
+        with open(Path(__file__).parent / "part1", "w") as opened_file:
+            opened_file.write(str(self.solve_part_1()))
+
+        with open(Path(__file__).parent / "part2", "w") as opened_file:
+            opened_file.write(str(self.solve_part_2()))
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    solution.save_results()
