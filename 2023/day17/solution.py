9217b2f71a546df36b0387416e46463025359ac9
Gaétan Ramet <gr@visium.ch>
2023-12-18 15:42:41 +0100

    refactor

--- 2023/day17/solution.py
+++ 2023/day17/solution.py
@@ -14,17 +14,18 @@ def get_next_states(pos_to_eval, visited, map_, **kwargs):
     next_pos = [
         ((pos_to_eval[1][0] + dir[0], pos_to_eval[1][1] + dir[1]), dir)
         for dir in deltas4_2d
-        if not pos_to_eval[2].count(dir) == 3
-        and dir != (-pos_to_eval[2][0][0], -pos_to_eval[2][0][1])
+        if dir != (-pos_to_eval[2][0], -pos_to_eval[2][1])
+        and not (dir == pos_to_eval[2] and pos_to_eval[3] == 3)
     ]
-    next_pos = [pos for pos in next_pos if pos[0] in map_]
     next_states = [
         (
             pos_to_eval[0] + map_[adj_pos[0]],
             adj_pos[0],
-            (adj_pos[1],) + pos_to_eval[2][:2],
+            adj_pos[1],
+            1 if adj_pos[1] != pos_to_eval[2] else pos_to_eval[3] + 1,
         )
         for adj_pos in next_pos
+        if adj_pos[0] in map_
     ]
     next_states = [
         state for state in next_states if visited.get(state[1:], 1e100) >= state[0]
@@ -33,8 +34,8 @@ def get_next_states(pos_to_eval, visited, map_, **kwargs):
 
 
 def get_next_states_part2(pos_to_eval, visited, map_, **kwargs):
-    last_dir = pos_to_eval[2][0]
-    if not all(dir == last_dir for dir in pos_to_eval[2][:4]):
+    last_dir = pos_to_eval[2]
+    if pos_to_eval[3] < 4:
         next_pos = [
             (
                 (pos_to_eval[1][0] + last_dir[0], pos_to_eval[1][1] + last_dir[1]),
@@ -45,17 +46,18 @@ def get_next_states_part2(pos_to_eval, visited, map_, **kwargs):
         next_pos = [
             ((pos_to_eval[1][0] + dir[0], pos_to_eval[1][1] + dir[1]), dir)
             for dir in deltas4_2d
-            if not pos_to_eval[2].count(dir) == 10
-            and dir != (-pos_to_eval[2][0][0], -pos_to_eval[2][0][1])
+            if dir != (-last_dir[0], -last_dir[1])
+            and not (dir == last_dir and pos_to_eval[3] == 10)
         ]
-    next_pos = [pos for pos in next_pos if pos[0] in map_]
     next_states = [
         (
             pos_to_eval[0] + map_[adj_pos[0]],
             adj_pos[0],
-            (adj_pos[1],) + pos_to_eval[2][:9],
+            adj_pos[1],
+            1 if adj_pos[1] != last_dir else pos_to_eval[3] + 1,
         )
         for adj_pos in next_pos
+        if adj_pos[0] in map_
     ]
     next_states = [
         state for state in next_states if visited.get(state[1:], 1e100) >= state[0]
@@ -78,18 +80,18 @@ class Solution:
         self.chr_map = {str(x): x for x in range(10)}
         self.map = ascii_image_to_map(self.input, self.chr_map)
         self.map_dim = max(self.map)
-        print(self.map)
 
     def reach_target(self, pos, **kwargs):
         return pos[1] == self.map_dim
 
     def reach_target_part2(self, pos, **kwargs):
-        last_dir = pos[2][0]
-
-        return pos[1] == self.map_dim and all(dir == last_dir for dir in pos[2][:4])
+        return pos[1] == self.map_dim and pos[3] >= 4
 
     def solve_part_1(self):
-        start_pos = [(0, (0, 0), ((0, 0),))]
+        start_pos = [
+            (self.map[(0, 1)], (0, 1), (0, 1), 1),
+            (self.map[(1, 0)], (1, 0), (1, 0), 1),
+        ]
         heapq.heapify(start_pos)
         answer = shortest_path_heap(
             start_pos,
@@ -105,7 +107,10 @@ class Solution:
         return answer
 
     def solve_part_2(self):
-        start_pos = [(0, (0, 0), ((0, 0),))]
+        start_pos = [
+            (self.map[(0, 1)], (0, 1), (0, 1), 1),
+            (self.map[(1, 0)], (1, 0), (1, 0), 1),
+        ]
         heapq.heapify(start_pos)
         answer = shortest_path_heap(
             start_pos,
